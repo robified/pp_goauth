d0eb067f90219544471dec8a751f47cb01a3775c
Robin <daorob@gmail.com>
2020-04-05 03:20:42 -0500

    Install/require the oauth strategy.

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,5 @@
 // this is not middleware, this is to configure passport
+// this is the same passport module like the one in server.js
 const passport = require('passport');
+// require oauth strategy module. this is capitalized because it's a class
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
