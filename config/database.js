2c66bc270f4ae1d5c9fcc3d366880ea5d7d2031d
Robin <daorob@gmail.com>
2020-04-05 07:58:15 -0500

    Able to connect to MongoDB Atlas with process.env variable.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/games', {
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
